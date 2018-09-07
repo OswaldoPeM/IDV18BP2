1d9a415c6702e6de99e63fe5f6055acf38dec389
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2018-09-06 23:09:18 -0500

    Tarea de JP Capitulo 2
    Capitulo 2

---
+++ Practicas/Practicas/Practicas.cpp
@@ -0,0 +1,95 @@
+#include "Includes.h"
+using namespace std;
+int WC()
+{
+	system("cls");
+	cout << "Accion No 1\nEl tocador se encuentra subiendo las escaleras ,que se encuentran a la derecha de la puerta de entrada, a mano izquierda al fondo";
+	cin.get();
+	cout << "Si te encuentras en la entrada de la casa volteas hacia la derecha donde estan las escaleras";
+	cin.get();
+	cout << "Subes los 30 escalones";
+	cin.get();
+	cout << "Una vez arriba, volteas a tu izquierda";
+	cin.get();
+	cout << "Caminas hasta el fondo, donde esta la puerta";
+	cin.get();
+	cout << "Abres la puerta";
+	cin.get();
+	cout << "Entras";
+	cin.get();
+	cout << "Cierras la puerta";
+	cin.get();
+	cout << "Te colocas en frente de  el escusado";
+	cin.get();
+	cout << "Abres la tapa";
+	cin.get();
+	cout << "***CENSURADO***";
+	cin.get();
+	cout << "Todo esta bien ahora. Estas bien y eso esta bien.\n\n\nAhora vamos a la escuela.";
+	cin.get();
+	return 0;
+}
+int rideSchool()
+{
+	system("cls");
+	cout << "Accion No 2\nEstas a punto de dirigirte a la escuela, habitas en Efrain Glz Luna, a media cuadra de chapultepec sur.";
+	cin.get();
+	cout << "Sales de casa.";
+	cin.get();
+	cout << "Subes al coche.";
+	cin.get();
+	cout << "Prendes el coche.";
+	cin.get();
+	cout << "Aceleras si no hay vehiculos de forma constante.";
+	cin.get();
+	cout << "Una vez llegas a chapultepec das vuelta a la derecha.";
+	cin.get();
+	cout << "A la primera oportunidad das vuelta en 'u'";
+	cin.get();
+	cout << "Te encaminas por chapultepec hasta que se acaba la Av";
+	cin.get();
+	cout << "Subes por Andres Teran hasta que llegas a la glorienta de los Ingenieros";
+	cin.get();
+	cout << "Buscas estacionamiento";
+	cin.get();
+	cout << "Una vez encuentras un lugar lo usas para estacionarte";
+	cin.get();
+	cout << "Caminas a la escuela y ya has llegado.\n\n\nAhora Haremos una receta perfecta.";
+	cin.get();
+	return 0;
+}
+int receta() 
+{
+	system("cls");
+	cout << "Accion No3\nVASO CON CEREAL CHOCOSCHOCOS\nIngredientes necesarios.\n~ChocosChocos\n~Leche\n~Vaso edicion especial de 'El Rey Leon'\n";
+	cin.get();
+	cout << "Pon el vaso en medio de la mesa";
+	cin.get();
+	cout << "Introduce los chocochocos en el vaso";
+	cin.get();
+	cout << "Deposita el liquido extraido de vacas violadas en el vaso lleno de cereales trangenicos con altos porsentajes de insectos.";
+	cin.get();
+	cout << "Disfruta una comida de dioses";
+	cin.get();
+	cout << "Deja de llorar porque no puedes olvidarla";
+	cin.get();
+	cout << "NOOOO!!!! alejate de la cuerda";
+	cin.get();
+	cout << "***Ahora eres una pignata***";
+	cin.get();
+	cout << "(Llena de autodesprecio y humor acido.)\n\nGracias\n";
+	cin.get();
+	return 0;
+}
+int main()
+{
+	cout << "Hola, Programando!\nAqui Vamos!\n\n\n\nAhora vamos a hacer 3 acciones\n1~Ir al Bagno\n2~Ir a la escuela\n3~Cocinar un manjar de los dioses.";
+	cin.get();
+	system("cls");
+	WC();
+	rideSchool();
+	receta();
+	keep_window_open();
+
+	return 0;
+}
