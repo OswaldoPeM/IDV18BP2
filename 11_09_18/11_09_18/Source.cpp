b24cedd8d1476fceec92d14cf790977db34e9977
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2018-09-11 23:07:06 -0500

    actualizacion de la tarea

--- 11_09_18/11_09_18/Source.cpp
+++ 11_09_18/11_09_18/Source.cpp
@@ -1,7 +1,132 @@
 #include "Includes.h"
 using namespace std;
+/*
 
+Try This(
+int main()
+{
+	char Nombre;
+	double Edad;
+	cout<<"Introduce tu nombre y tu edad.(preciona enter al terminar)\n";
+	cin >> Nombre, Edad;
+	cout << "Hola " << Nombre << ", asi que tienes " << Edad*12 << " meses. Genial.\n";
 
+	retrun 0;
+}
+
+int main()
+{
+	cout << "Please enter a floating-point value: ";
+	double n;
+	cin >> n;
+	int entero,raiz;
+	entero = n;
+	cout << "n == " << entero
+	     << "\nn+1 == " << entero+1
+	     << "\nthree times n == " << 3*entero
+	     << "\ntwice n == " << entero*2
+	     << "\nn squared == " << entero*entero
+	     << "\nhalf of n == " << entero/2
+	     << "\nsquare root of n == ";
+	 raiz = sqrt(n)
+	 cout << raiz << "\n"; 
+}
+int main()
+{
+	string previous = " "; 
+	string current;
+	while (cin>>current)
+	{ 	if (previous == current)
+		{	
+			cout << "Palabra repetida: " << current << '\n';
+		}
+		previous = current;
+	}
+}
+int Main() //error: 'Main' instead 'main'
+{
+	STRING s = "Goodbye, cruel world! "; // hay un error en el tipo de dato.
+
+	cOut << S << '\n'; // 2 errors: 'cOut' instead 'cout', 'S' instead 's'
+}
+int main()
+{
+	double d = 0;
+	while (cin>>d) 
+	{ 
+		
+		int i = d;
+		char c = i; 
+		int i2 = c; 
+		cout << "d==" << d 
+		<< " i=="<< i 
+		<< " i2==" << i2 
+		<< " char(" << c << ")\n"; 
+	}
+}
+)
+*/
+/*
+int main()
+{
+	double km = 1.609;
+	double miles;
+	cout << "Introdusca las millas que quiere tranformar en kilometros.\n";
+	cin >> miles;
+	miles *= km;
+	cout << "Son " << miles << " kilometros.\n";
+	keep_window_open();
+}
+
+int main()
+{
+	int val1, val2;
+	float division = 0;
+	cout << "Ingrese dos valores enteros:\n";
+	cin >> val1, val2;
+	if (val1 > val2)
+		cout << val1 << "es màs grande\n";
+	if (val1 < val2)
+		cout << val2 << " es màs grande\n";
+	cout << "La suma es: " << val1 + val2 <<"\n";
+	cout << "La resta es: " << val1 - val2 <<"\n";
+	cout << "La multiplicacion es: " << val1 * val2 <<"\n";
+	division = val1 / val2;
+	if (val2 == 0)
+	{
+		cout<<"osea.... no se puede eso\n";
+	}
+	if (val2 != 0)
+	{
+		cout << "La divisiòn es: " << division << "\n";
+	}
+	keep_window_open();
+}
+
+int main()
+{
+	float val1, val2, division = 0;
+	cout << "Ingrese dos valores enteros:\n";
+	cin >> val1, val2;
+	if (val1 > val2)
+		cout << val1 << "es mas grande\n";
+	if (val1 < val2)
+		cout << val2 << " es mas grande\n";
+	cout << "La suma es: " << val1 + val2 <<"\n";
+	cout << "La resta es: " << val1 - val2 <<"\n";
+	cout << "La multiplicacion es: " << val1 * val2 <<"\n";
+	division = val1 / val2;
+	if (val2 == 0.0000)
+	{
+		cout<<"osea.... no se puede eso\n";
+	}
+	if (val2 != 0.0000)
+	{
+		cout << "La divisiòn es: " << division << "\n";
+	}
+	keep_window_open();
+}
+*/
 int main()
 {
 	cout << "Introduce tu nombre.(al terminar preciona la tecla enter).\n";
@@ -40,7 +165,6 @@ int main()
 	{
 		cout << "Disfruta el retiro, mi generacion no lo hara porque al parecer la productividad impera sobre la calidad de vida en este mundo de mierda.\n             Siempre tu amigo.\n\nOswaldo.\n";
 	}
-	cin.ignore();
-	cin.get();
+	keep_window_open();
 	return 0;
 }
